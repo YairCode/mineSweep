75f14eec8ad933d0655f617d6bb231d98c2a8af9
YairCode <118843321+YairCode@users.noreply.github.com>
2022-11-23 20:54:13 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,89 +17,91 @@ var clickPos
 var timer
 var negCount
 
-var gBallOnBoardCounter = 3
-var gBallInterval
-var gGlueInterval
+// function initGame(){
 
 gBoard = buildBoard()
-negsMineInsert()
+
+console.log('gBoard', gBoard)
+sendMinesCount(gBoard)
 renderBoard(gBoard, '.board-container')
 
-function cellClicked(elClickedNum, cell) {
-    var elCount = document.querySelector('.counter')
-    if (cell !== gNextNum) return
-    if (cell === 1) gInterval = setInterval(timer, 1000)
-
-    if (cell >= gLevel) {
-        elCount.innerText = '😁'
-        clearInterval(gInterval)
-        return
-    }
-    gNextNum++
-    elClickedNum.classList.add('locked')
-    elCount.innerText = gNextNum
-
-}
 
 
 
-function negsMineInsert(){
-for (var i = 0; i < gBoard.length; i++) {
-    for (var j = 0; j < gBoard[0].length; j++) 
 
-gBoard[i][j].minesAroundCount = countMines(i,j, gBoard)}
-}
-function setMinesNegsCount(board){
-}
+
+
+// negsMineInsert()
+
+
+
+
+
+
+
+// function negsMineInsert() {
+//     for (var i = 0; i < gBoard.length; i++) {
+//         for (var j = 0; j < gBoard[0].length; j++)
+
+//          gBoard[i][j].minesAroundCount = countMines(i, j, gBoard)
+// }
+
+//     }
+
+// function setMinesNegsCount(board) {}
 
 
 function buildBoard() {
     const board = []
     const rowCount = 4
     const colCount = 4
-
-
-
+    
     for (var i = 0; i < rowCount; i++) {
-
         board[i] = []
         for (var j = 0; j < colCount; j++) {
             board[i][j] = {
                 minesAroundCount: 0,
                 isShown: false, isMine: false, isMarked: false
             }
-
         }
     }
-    
+
     board[0][0].isMine = MINE
-    board[3][2].isMine = MINE
-setMinesNegsCount()
+    board[1][1].isMine = MINE
     return board
 }
 
 
-function countMines(cellI, cellJ, mat) {
+function minesAroundCount(board) {
+    var cellI = board[i]
+    var cellJ = board[j]
     var minesCount = 0
     for (var i = cellI - 1; i <= cellI + 1; i++) {
-        if (i < 0 || i >= mat.length) continue
+        if (i < 0 || i >= board.length) continue
         for (var j = cellJ - 1; j <= cellJ + 1; j++) {
             if (i === cellI && j === cellJ) continue
-            if (j < 0 || j >= mat[i].length) continue
-            if (mat[i][j].isMine === MINE) minesCount++
-           
-            
-
+            if (j < 0 || j >= board[i].length) continue
+            if (board[i][j].isMine === MINE) minesCount++
         }
 
     }
+    
     return minesCount
+}
+
+function sendMinesCount(board){
+    var minesCount = 0
+    board[0][0].minesAroundCount = minesCount
+
+
 }
 
 
 
 function renderBoard(mat, selector) {
 
+    
+    
     var strHTML = '<table border="0"><tbody>'
     for (var i = 0; i < mat.length; i++) {
 
@@ -108,9 +110,15 @@ function renderBoard(mat, selector) {
 
             const cell = mat[i][j]
             const className = `cell cell-${i}-${j}`
+               
+            strHTML += `<td onclick='this' class="${className}${gBoard[i][j].isMine}
+            ${gBoard[i][j].minesAroundCount}">Place</td>`
 
-            strHTML += `<td class="${className}${gBoard[i][j].isMine}
-            ${gBoard[i][j].minesAroundCount}"></td>`
+            
+
+
+            
+            
 
         }
         strHTML += '</tr>'
@@ -122,11 +130,13 @@ function renderBoard(mat, selector) {
 }
 
 
-// build 4X4 martix position two mines manually
+
 
 // create neg loop that counts arround clickable area and gives number in cell
 
 
 //on click turn tile (like numbers game)
+
+
 
 
