4388dd5c01a8e84f5cf58adc418d6d126ac2d033
YairCode <118843321+YairCode@users.noreply.github.com>
2022-11-24 20:17:29 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -13,95 +13,27 @@ const WIN = '😎'
 var mineCount = 0
 var gLevel
 var gBoard
-var clickPos
+var cellPos
 var timer
 var negCount
+var gCellPos
 
 // function initGame(){
 
 gBoard = buildBoard()
+console.log(gBoard)
 
-console.log('gBoard', gBoard)
-sendMinesCount(gBoard)
+
+
+// minesAroundCount(gBoard)
+
+
 renderBoard(gBoard, '.board-container')
-
-
-
-
-
-
-
-// negsMineInsert()
-
-
-
-
-
-
-
-// function negsMineInsert() {
-//     for (var i = 0; i < gBoard.length; i++) {
-//         for (var j = 0; j < gBoard[0].length; j++)
-
-//          gBoard[i][j].minesAroundCount = countMines(i, j, gBoard)
-// }
-
-//     }
-
-// function setMinesNegsCount(board) {}
-
-
-function buildBoard() {
-    const board = []
-    const rowCount = 4
-    const colCount = 4
-    
-    for (var i = 0; i < rowCount; i++) {
-        board[i] = []
-        for (var j = 0; j < colCount; j++) {
-            board[i][j] = {
-                minesAroundCount: 0,
-                isShown: false, isMine: false, isMarked: false
-            }
-        }
-    }
-
-    board[0][0].isMine = MINE
-    board[1][1].isMine = MINE
-    return board
-}
-
-
-function minesAroundCount(board) {
-    var cellI = board[i]
-    var cellJ = board[j]
-    var minesCount = 0
-    for (var i = cellI - 1; i <= cellI + 1; i++) {
-        if (i < 0 || i >= board.length) continue
-        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
-            if (i === cellI && j === cellJ) continue
-            if (j < 0 || j >= board[i].length) continue
-            if (board[i][j].isMine === MINE) minesCount++
-        }
-
-    }
-    
-    return minesCount
-}
-
-function sendMinesCount(board){
-    var minesCount = 0
-    board[0][0].minesAroundCount = minesCount
-
-
-}
 
 
 
 function renderBoard(mat, selector) {
 
-    
-    
     var strHTML = '<table border="0"><tbody>'
     for (var i = 0; i < mat.length; i++) {
 
@@ -110,16 +42,20 @@ function renderBoard(mat, selector) {
 
             const cell = mat[i][j]
             const className = `cell cell-${i}-${j}`
-               
-            strHTML += `<td onclick='this' class="${className}${gBoard[i][j].isMine}
-            ${gBoard[i][j].minesAroundCount}">Place</td>`
-
-            
 
 
-            
-            
+            var innerText = `${countNeighbors(mat[i][j].i, mat[i][j].j, gBoard)}`
+          
+                  
+            if (mat[i][j].isMine) {
+                innerText = MINE
+            } else {
+                innerText = `${countNeighbors(mat[i][j].i, mat[i][j].j, gBoard)}`
+            }
 
+            strHTML += `<td class = "cell" onclick='{clickedCell()}'
+             ${mat[i][j].isMine} id="{i:${mat[i][j].i}-j:${mat[i][j].j}\}">
+            ${innerText}</td>`
         }
         strHTML += '</tr>'
     }
@@ -129,14 +65,70 @@ function renderBoard(mat, selector) {
     elContainer.innerHTML = strHTML
 }
 
+var cellInnerText = document.querySelector("#\\{i\\:0-j\\:0\\}")
 
 
 
-// create neg loop that counts arround clickable area and gives number in cell
 
 
-//on click turn tile (like numbers game)
+function buildBoard() {
+    const board = []
+    const rowCount = 4
+    const colCount = 4
 
+    for (var i = 0; i < rowCount; i++) {
+        board[i] = []
+        for (var j = 0; j < colCount; j++) {
+            board[i][j] = {
+                i: i, j: j, minesAroundCount: 0,
+                isShown: false, isMine: false, isMarked: false
+            }
+        }
+    }
+    board[0][0].isMine = MINE
+    board[3][3].isMine = MINE
+    board[0][1].isMine = MINE
+
+    return board
+}
+setMinesNegsCount()
+
+function setMinesNegsCount() {
+
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j; j < gBoard[0].length; j++) {
+            var cellI = gBoard[i][j].i
+            var cellJ = gBoard[i][j].j
+
+
+        }
+    }
+
+}
+
+var elSelctor = document.querySelector('.board-conatainer')
+console.log('td', elSelctor)
+
+console.log('getCell', getCell.id)
+
+
+var mineCount = countNeighbors(1, 0, gBoard)
+console.log('mineCount', mineCount)
+
+function countNeighbors(cellI, cellJ, board) {
+    var neighborsCount = 0;
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= board.length) continue;
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (j < 0 || j >= board[i].length) continue;
+            if (i === cellI && j === cellJ) continue;
+            if (board[i][j].isMine) neighborsCount++;
+        }
+    }
+    return (neighborsCount>0?neighborsCount:'' );
+}
+
+document.querySelector("#\\{i\\:0-j\\:0\\}")
 
 
 
